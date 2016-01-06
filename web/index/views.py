ecb22969dd4602c50164be37a51618fe3b2f1cf5
Narendra Nath Joshi <narendranathjoshi@gmail.com>
2016-01-07 00:42:44 +0530

    Home page changes

--- web/index/views.py
+++ web/index/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import View
+
+
+class HomeView(View):
+    def get(self, request):
+        return render(request, "home.html")
--- web/web/settings.py
+++ web/web/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'index',
+    'blog',
+    'analytics',
+    'books'
 ]
 
 MIDDLEWARE_CLASSES = [
