a1a7a9e6f6f759f9d190598dd981b1204322db86
Narendra Nath Joshi <narendranathjoshi@gmail.com>
2016-01-10 18:47:34 +0530

    Added Work page

--- web/index/urls.py
+++ web/index/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from django.views.generic import RedirectView
 from index.views import HomeView
+from index.views import HomeView, BlogView ,WorkView
 
 urlpatterns = [
     url('', HomeView.as_view())
+    url(r'^work/$', WorkView.as_view())
 ]
