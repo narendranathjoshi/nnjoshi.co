95a27c50d06d984b1d5f877b0248e180433fb836
Narendra Nath Joshi <narendranathjoshi@gmail.com>
2016-01-10 23:03:06 +0530

    Modified STATIC_ROOT

--- web/web/settings.py
+++ web/web/settings.py
@@ -145,4 +145,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/home/narendranathjoshi/nnjoshi.co/web/index/static'
+STATIC_ROOT = '/home/narendranathjoshi/nnjoshi.co/web/index/static/'
