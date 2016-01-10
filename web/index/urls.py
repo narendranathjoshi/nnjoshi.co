ab8881296f8d0058d292dbede252e287cb04dfce
Narendra Nath Joshi <narendranathjoshi@gmail.com>
2016-01-10 18:49:31 +0530

    Added Home and Blog pages, auto-redirect to blog setup

--- web/index/urls.py
+++ web/index/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from django.views.generic import RedirectView
-from index.views import HomeView
+from django.views.generic.base import RedirectView
 from index.views import HomeView, BlogView ,WorkView
 
 urlpatterns = [
-    url('', HomeView.as_view())
+    url(r'^about/$', HomeView.as_view()),
+    url(r'^$', RedirectView.as_view(url='/blog/', permanent=False)),
+    url(r'^blog/$', BlogView.as_view()),
     url(r'^work/$', WorkView.as_view())
 ]
--- web/index/views.py
+++ web/index/views.py
@@ -2,9 +2,32 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.template.loader import get_template
 from django.views.generic import View
+
+
+def render_nav_page(page_type):
+    return get_template("nav.html").render({
+        "page": page_type
+    })
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, "home.html")
+        return render(request, "home.html", {
+            "render_nav_page": render_nav_page
+        })
+
+
+class BlogView(View):
+    def get(self, request):
+        return render(request, "blog.html", {
+            "render_nav_page": render_nav_page
+        })
+
+
+class WorkView(View):
+    def get(self, request):
+        return render(request, "work.html", {
+            "render_nav_page": render_nav_page
+        })
