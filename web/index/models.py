8ca02b3c66c70ffaf970a669e235c42abf04083d
Narendra Nath Joshi <narendranathjoshi@gmail.com>
2016-01-10 19:15:44 +0530

    Added initial models

--- web/index/models.py
+++ web/index/models.py
@@ -3,3 +3,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class BlogEntry(models.Model):
+    title = models.CharField(max_length=150)
+    tags = models.ManyToManyField(Tag)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
