263bb8b7573691d804448c9f24c733812f7f3ea0
Narendra Nath Joshi <narendranathjoshi@gmail.com>
2016-01-10 19:16:13 +0530

    Added admin views for models

--- web/index/admin.py
+++ web/index/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from index.models import BlogEntry, Tag
+
+
+class BlogEntryAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "created"]
+    search_fields = ["title", "tags"]
+    list_filter = ["tags", "created"]
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "created"]
+    search_fields = ["title"]
+    list_filter = ["created"]
+
+admin.site.register(BlogEntry, BlogEntryAdmin)
+admin.site.register(Tag, TagAdmin)
