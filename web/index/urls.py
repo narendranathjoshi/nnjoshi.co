4f9450575b84e9a69b1d97c4bee0ba321b51cfb5
Narendra Nath Joshi <narendranathjoshi@gmail.com>
2016-01-11 23:05:38 +0530

    Home is now blog

--- web/index/urls.py
+++ web/index/urls.py
@@ -3,8 +3,7 @@ from django.views.generic.base import RedirectView
 from index.views import HomeView, BlogView ,WorkView
 
 urlpatterns = [
+    url('', BlogView.as_view()),
     url(r'^about/$', HomeView.as_view()),
-    url(r'^$', RedirectView.as_view(url='/blog/', permanent=True)),
-    url(r'^blog/$', BlogView.as_view()),
     url(r'^work/$', WorkView.as_view())
 ]
