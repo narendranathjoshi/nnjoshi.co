f9ea865fddc4a5fbd1c9e284037c95847e612448
Narendra Nath Joshi <narendranathjoshi@gmail.com>
2016-01-11 23:09:13 +0530

    URLs fix

--- web/index/urls.py
+++ web/index/urls.py
@@ -3,7 +3,7 @@ from django.views.generic.base import RedirectView
 from index.views import HomeView, BlogView ,WorkView
 
 urlpatterns = [
-    url('$', BlogView.as_view()),
+    url(r'^/$', BlogView.as_view()),
     url(r'^about/$', HomeView.as_view()),
     url(r'^work/$', WorkView.as_view())
 ]
