e9144b264e79dfa50c1039b399b9170a765b5752
Narendra Nath Joshi <narendranathjoshi@gmail.com>
2016-01-13 23:22:41 +0530

    Added a sneak peek field for BlogEntry

--- web/index/models.py
+++ web/index/models.py
@@ -19,6 +19,7 @@ class BlogEntry(models.Model):
     slug = models.SlugField(auto_created=True, unique=True, db_index=True)
     image = models.CharField(max_length=100, blank=True, null=True)
     entry = models.TextField(default='')
+    peek = models.CharField(max_length=640, default='')
     tags = models.ManyToManyField(Tag)
     created = models.DateTimeField(auto_now_add=True)
 
