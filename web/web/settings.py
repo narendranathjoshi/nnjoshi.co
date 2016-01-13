1cb36ec136a60d13010396180dc576f4e8b3f4c5
Narendra Nath Joshi <narendranathjoshi@gmail.com>
2016-01-13 22:00:52 +0530

    Removed references to analytics app

--- web/web/settings.py
+++ web/web/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'index',
-    'analytics',
 ]
 
 MIDDLEWARE_CLASSES = [
--- web/web/urls.py
+++ web/web/urls.py
@@ -19,5 +19,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('', include('index.urls')),
-    url(r'^analytics/', include('analytics.urls')),
 ]
