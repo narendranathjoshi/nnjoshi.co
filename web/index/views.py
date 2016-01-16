b200dd80334adfcc25a04dbb27d57186603aa357
Narendra Nath Joshi <narendranathjoshi@gmail.com>
2016-01-16 12:26:32 +0530

    Added auto save and other display views

--- web/index/views.py
+++ web/index/views.py
@@ -1,12 +1,13 @@
 import logging
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 from django.template.loader import get_template
+from django.utils.text import slugify
 from django.views.generic import View
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
-from index.models import Tag
+from index.models import Tag, BlogEntry
 from index.serializers import BlogEntrySerializer
 
 logger = logging.getLogger(__name__)
@@ -21,14 +22,35 @@ def get_local_log(msg):
     return "(LOCAL DEV): %s" % msg
 
 
+def auto_save(data):
+        slug = slugify(data["title"])
+        if slug and slug != '':
+            blog_entry, created = BlogEntry.objects.get_or_create(slug=slug)
+            blog_entry.title = data["title"]
+            blog_entry.image = data["image"]
+            blog_entry.image_caption = data["image_caption"]
+            blog_entry.entry = data["entry"]
+            blog_entry.peek = data["entry"][:600]
+            for tag_id in data["tags"]:
+                blog_entry.tags.add(Tag.objects.get(id=tag_id))
+            if data["new_tag"] != '':
+                new_tag, _ = Tag.objects.get_or_create(
+                    slug=slugify(data["new_tag"]))
+                new_tag.title = data["new_tag"]
+                new_tag.save()
+                blog_entry.tags.add(new_tag)
+            blog_entry.save()
+
+            return blog_entry
+        else:
+            return None
+
+
 def render_blog_entry(data):
-    data["tags"] = [Tag.objects.get(id=tag_id) for tag_id in data["tags"]]
     return get_template("entry_template.html.j").render(data)
 
 
 def render_blog_peek(data):
-    data["tags"] = [Tag.objects.get(id=tag_id) for tag_id in data["tags"]]
-    data["peek"] = data["entry"][:600]
     return get_template("peek_template.html.j").render(data)
 
 
@@ -44,6 +66,16 @@ def handle_uploaded_file(f, file_save_path):
             destination.write(chunk)
 
 
+def qdict_to_dict(qdict):
+    """Convert a Django QueryDict to a Python dict.
+
+    Single-value fields are put in directly, and for multi-value fields, a list
+    of all values is stored at the field's key.
+
+    """
+    return {k: v[0] if len(v) == 1 else v for k, v in qdict.lists()}
+
+
 # Views from here
 class HomeView(View):
     def get(self, request):
@@ -57,6 +89,27 @@ class BlogView(View):
 
         return render(request, "blog.html.j", {
             "render_nav_page": render_nav_page
+        })
+
+
+class BlogEntryView(View):
+    def get(self, request, slug):
+        blog_entry = BlogEntry.objects.get(slug=slug)
+
+        return render(request, "blog.html.j", {
+            "render_nav_page": render_nav_page,
+            "blog_entry": blog_entry
+        })
+
+
+class TaggedView(View):
+    def get(self, request, slug):
+        tag = Tag.objects.get(slug=slug)
+        blog_entries = BlogEntry.objects.filter(tags=tag)
+
+        return render(request, "blog.html.j", {
+            "render_nav_page": render_nav_page,
+            "blog_entries": blog_entries
         })
 
 
@@ -76,6 +129,15 @@ class BlogWriteView(View):
             "render_blog_entry": render_blog_entry,
         })
 
+    def post(self, request):
+        data = qdict_to_dict(request.POST)
+        blog_entry = auto_save(data)
+        blog_entry.is_published = True
+        blog_entry.save()
+
+        return HttpResponseRedirect("/blog/%s" % blog_entry.slug)
+
+
 
 # API Views
 class PreviewAPIView(ListAPIView):
@@ -83,7 +145,23 @@ class PreviewAPIView(ListAPIView):
     serializer_class = BlogEntrySerializer
 
     def post(self, request, *args, **kwargs):
+        data = qdict_to_dict(request.data)
+        blog_entry = auto_save(data)
+
         return Response({
-            "peek": render_blog_peek(request.data),
-            "entry": render_blog_entry(request.data)
+            "peek": render_blog_peek({"blog_entry": blog_entry}),
+            "entry": render_blog_entry({"blog_entry": blog_entry})
+        })
+
+
+class AutoSaveAPIView(ListAPIView):
+    queryset = []
+    serializer_class = BlogEntrySerializer
+
+    def post(self, request, *args, **kwargs):
+        data = qdict_to_dict(request.data)
+        auto_save(data)
+
+        return Response({
+            "success": True
         })
