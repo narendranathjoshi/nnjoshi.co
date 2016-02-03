bc3149bd5952d9688272b13fc870bff08fa94079
Narendra Nath Joshi <narendranathjoshi@gmail.com>
2016-02-04 00:02:59 +0530

    Migrations issue

--- web/index/models.py
+++ web/index/models.py
@@ -31,12 +31,3 @@ class BlogEntry(models.Model):
 
     def __str__(self):
         return self.title
-
-
-class Subscriber(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    email = models.EmailField(default='', unique=True)
-    dummy = models.EmailField(default='', unique=True)
-
-    def __str__(self):
-        return self.email
