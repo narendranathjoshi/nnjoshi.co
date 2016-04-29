b3ad841e7e46cd28335116b146b3aef6cc1e5fe1
Narendra Nath Joshi <me@nnjoshi.co>
2016-04-29 15:06:16 +0530

    Dummy landing page for website

--- web/resume/views.py
+++ web/resume/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import View
+
+
+class DummyHomeView(View):
+    def get(self, request):
+        return HttpResponse("Yes, it works!")
