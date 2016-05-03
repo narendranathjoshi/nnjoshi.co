89e9d3aecae4cf0ab15c1a8630fa508a8c1a0b9a
Narendra Nath Joshi <me@nnjoshi.co>
2016-05-03 15:11:58 +0530

    Added resume page

--- web/resume/views.py
+++ web/resume/views.py
@@ -3,8 +3,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import View
+from resume.models import *
 
 
 class DummyHomeView(View):
     def get(self, request):
-        return HttpResponse("Yes, it works!")
+        email = BasicInfo.objects.get(key="email")
+        phone = BasicInfo.objects.get(key="phone")
+        skills = Skill.objects.all()
+        achievements = Achievement.objects.all()
+        projects = Project.objects.all().order_by("-year")
+        educations = Education.objects.all().order_by("-start_date")
+        experiences = Experience.objects.all().order_by("-start_date")
+
+        return render(request, "index.html.jinja", {
+            "email": email,
+            "phone": phone,
+            "skills": skills,
+            "achievements": achievements,
+            "projects": projects,
+            "educations": educations,
+            "experiences": experiences,
+        })
