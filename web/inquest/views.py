1a35638e638d9414c01e3ccfcdb2ccac08f3f6da
Narendra Nath Joshi <me@nnjoshi.co>
2016-05-13 23:15:41 +0530

    Added top header

--- web/inquest/views.py
+++ web/inquest/views.py
@@ -1,10 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import View
-from resume.models import *
-from web.rediscache import resume_cache
 
 
 class HomeView(View):
