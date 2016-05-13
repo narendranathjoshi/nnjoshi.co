dc05b5a599d7f4dd65344219a80139e9b08bea49
Narendra Nath Joshi <me@nnjoshi.co>
2016-05-13 23:22:16 +0530

    Added admin for Post model

--- web/blog/admin.py
+++ web/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from blog.models import *
+
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "created", "is_published")
+    list_filter = ("created", "is_published")
+
+
+admin.site.register(Post, PostAdmin)
