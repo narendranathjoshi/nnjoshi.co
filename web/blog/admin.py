4304e5e645c171a26b50c2005fc82148fafe331c
Narendra Nath Joshi <me@nnjoshi.co>
2016-05-31 22:54:58 +0530

    Fix admin action add

--- web/blog/admin.py
+++ web/blog/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from blog.models import *
 
 
+def publish_post(modeladmin, request, queryset):
+    queryset.update(is_published=True)
+
+publish_post.short_description = "Publish post to blog"
+
+
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "created", "is_published")
@@ -12,7 +18,3 @@ class PostAdmin(admin.ModelAdmin):
 admin.site.register(Post, PostAdmin)
 
 
-def publish_post(modeladmin, request, queryset):
-    queryset.update(is_published=True)
-
-publish_post.short_description = "Publish post to blog"
