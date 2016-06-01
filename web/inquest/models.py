ad10cff744d375c97232e6b5d847420c208d1e1f
Narendra Nath Joshi <me@nnjoshi.co>
2016-06-01 23:29:53 +0530

    Added UserInfo model

--- web/inquest/models.py
+++ web/inquest/models.py
@@ -2,4 +2,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class UserInfo(models.Model):
+    username = models.CharField(max_length=200)
+    os = models.CharField(max_length=200)
+    sdk = models.CharField(max_length=200)
+    device = models.CharField(max_length=200)
+    build = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+    last_ping = models.DateTimeField()
+
+    def __str__(self):
+        return self.username
