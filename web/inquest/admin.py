4cb3e7b1071aeacdf7d61777c7d0869f11d1a442
Narendra Nath Joshi <me@nnjoshi.co>
2016-06-01 23:30:07 +0530

    Admin for UserInfo model

--- web/inquest/admin.py
+++ web/inquest/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from inquest.models import UserInfo
+
+
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "os", "sdk", "device", "build",
+                    "is_active", "last_ping")
+    list_filter = ("os", "sdk", "device", "build", "is_active")
+
+
+admin.site.register(UserInfo, UserInfoAdmin)
