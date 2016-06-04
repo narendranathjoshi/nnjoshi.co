fdd1adcab6b11e283c70db455fdd2c833f591c43
Narendra Nath Joshi <me@nnjoshi.co>
2016-06-04 16:07:46 +0530

    Added is_pro field

--- web/inquest/models.py
+++ web/inquest/models.py
@@ -1,4 +1,6 @@
 from __future__ import unicode_literals
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 
 from django.db import models
 
@@ -11,7 +13,16 @@ class UserInfo(models.Model):
     device = models.CharField(max_length=200)
     build = models.CharField(max_length=200)
     is_active = models.BooleanField(default=True)
+    is_pro = models.BooleanField(default=False)
     last_ping = models.DateTimeField()
+
+    def _is_email(self):
+        try:
+            validate_email(self.username)
+            return True
+        except ValidationError:
+            return False
+    is_email = property(_is_email)
 
     def __str__(self):
         return self.username
