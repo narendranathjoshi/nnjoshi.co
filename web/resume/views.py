1cdfa98d138158d8cf0e6508a50c4bf9c012b6de
Narendra Nath Joshi <me@nnjoshi.co>
2016-08-30 19:48:36 -0400

    Disabled cache temporarily

--- web/resume/views.py
+++ web/resume/views.py
@@ -1,30 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-# Create your views here.
 from django.views.generic import View
+
 from resume.models import *
-from web.rediscache import resume_cache
 
 
 class HomeView(View):
     def get(self, request):
-        info_cache_key = "info_v1"
-        info = resume_cache.pickled_get(info_cache_key)
-
-        if not info:
-            info = {
-                "email": BasicInfo.objects.get(key="email").value,
-                "phone": BasicInfo.objects.get(key="phone").value,
-                "head_bar": BasicInfo.objects.get(key="head_bar").value,
-                "skills": Skill.objects.all(),
-                "achievements": Achievement.objects.all(),
-                "projects": Project.objects.all().order_by("-year"),
-                "educations": Education.objects.all().order_by("-start_date"),
-                "experiences": Experience.objects.all().order_by("-start_date")
-            }
-
-            resume_cache.pickled_set(info_cache_key, info, 24 * 60 * 60 * 1000)
+        info = {
+            "email": BasicInfo.objects.get(key="email").value,
+            "phone": BasicInfo.objects.get(key="phone").value,
+            "head_bar": BasicInfo.objects.get(key="head_bar").value,
+            "skills": Skill.objects.all(),
+            "achievements": Achievement.objects.all(),
+            "projects": Project.objects.all().order_by("-year"),
+            "educations": Education.objects.all().order_by("-start_date"),
+            "experiences": Experience.objects.all().order_by("-start_date")
+        }
 
         return render(request, "index.html.jinja", {
             "email": info["email"],
